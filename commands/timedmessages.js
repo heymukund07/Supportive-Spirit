a891408629f7fc39b9275198d8cef6267b69018d
Mukund Gupta <124252909+heymukund07@users.noreply.github.com>
2023-12-26 19:10:08 +0530

    main

--- commands/timedmessages.js
+++ commands/timedmessages.js
@@ -1,15 +1,36 @@
-const botchatid = "838764293242880042"
+const botchatid = "838764293242880042";
 const fs = require('fs');
 const path = require('path');
-module.exports = (bot)=>{ 
-    var channel = bot.channels.cache.find(c=> c.id === botchatid)
 
-    let rawdata = fs.readFileSync(path.resolve(__dirname, 'timedmessagescontent.json'));
-    let data = JSON.parse(rawdata);
-    const messages = data.messagecontent
-    var msize = messages.length
-    const randomNum = (Math.floor(Math.random()* msize)); 
-    var msg = messages[randomNum]
+module.exports = (bot) => {
+    try {
+        // Find the channel with the specified ID
+        var channel = bot.channels.cache.find(c => c.id === botchatid);
 
-    channel.send(msg)
-}
+        // Check if the channel is valid
+        if (!channel) {
+            console.error(`Channel with ID ${botchatid} not found.`);
+            return;
+        }
+
+        // Read timed messages from the JSON file
+        let rawdata = fs.readFileSync(path.resolve(__dirname, 'timedmessagescontent.json'));
+        let data = JSON.parse(rawdata);
+        const messages = data.messagecontent;
+
+        // Check if there are messages in the array
+        if (messages.length === 0) {
+            console.error("No timed messages found in the JSON file.");
+            return;
+        }
+
+        // Generate a random index to select a message
+        const randomNum = Math.floor(Math.random() * messages.length);
+        var msg = messages[randomNum];
+
+        // Send the message to the channel
+        channel.send(msg);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+};
